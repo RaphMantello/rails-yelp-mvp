ed841625af108f53e95d3d30438f0ea43dcc180c
Raphael Mantello <mantello.raphael@gmail.com>
2019-05-20 12:16:39 +0200

    work in progress

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(french chinese italian japanese belgian) }
+end
